8ade7f6b7c364936c8424a12d1699a6c1ef1fbe9
opifex <opifexM@outlook.com>
2023-06-26 18:01:57 -0400

    added json and plain formats

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import readFileData from './fileReader.js';
 import getDiffData from './parsers.js';
 import getDiffReport from './formatters/index.js';
 
-const startGeneration = (file1, file2, format) => {
+const startGeneration = (file1, file2, format = 'stylish') => {
   const data1 = readFileData(file1);
   const data2 = readFileData(file2);
   const diffData = getDiffData(data1, data2);
