8506aa0a1f27e2df951a6351fc2186ca2a37fd5d
opifex <opifexM@outlook.com>
2023-06-26 19:06:40 -0400

    refactoring

--- src/parsers.js
+++ src/parsers.js
@@ -2,14 +2,9 @@ import _ from 'lodash';
 
 const getDiffData = (data1, data2) => {
   const keys = [...Object.keys(data1), ...Object.keys(data2)];
-  const uniqueKeys = Array
-    .from(new Set(keys))
-    .slice()
-    .sort((a, b) => a.localeCompare(b));
-  const diffMap = new Map();
+  const uniqueKeys = _.sortBy(Array.from(new Set(keys)), (key) => key);
 
-  for (let i = 0; i < uniqueKeys.length; i += 1) {
-    const key = uniqueKeys[i];
+  return uniqueKeys.reduce((map, key) => {
     const hasInData1 = Object.hasOwn(data1, key);
     const hasInData2 = Object.hasOwn(data2, key);
     const value1 = hasInData1 ? data1[key] : '';
@@ -17,18 +12,18 @@ const getDiffData = (data1, data2) => {
     const isSameValues = (hasInData1 && hasInData2) ? _.isEqual(value1, value2) : false;
 
     if (hasInData1 && !hasInData2) {
-      diffMap.set(key, 'deleted');
+      map.set(key, 'deleted');
     } else if (!hasInData1 && hasInData2) {
-      diffMap.set(key, 'added');
+      map.set(key, 'added');
     } else if (hasInData1 && hasInData2 && isSameValues) {
-      diffMap.set(key, 'unchanged');
+      map.set(key, 'unchanged');
     } else if (_.isObject(value1) && _.isObject(value2)) {
-      diffMap.set(key, getDiffData(value1, value2));
+      map.set(key, getDiffData(value1, value2));
     } else {
-      diffMap.set(key, 'changed');
+      map.set(key, 'changed');
     }
-  }
-  return diffMap;
+    return map;
+  }, new Map());
 };
 
 export default getDiffData;
