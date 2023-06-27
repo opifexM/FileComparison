6728bf14e719bbac9189c081167fa351e705cf69
opifex <opifexM@outlook.com>
2023-06-27 10:21:05 -0400

    refactoring

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,57 +2,60 @@ import _ from 'lodash';
 
 const addStartEndStylish = (diffReport) => `{\n${diffReport}}`;
 
-const SPACE_NAMBER = 4;
+const SPACE_INCREMENT = 4;
 const getFormatObject = (obj, spaceNumber = 0) => {
   if (!_.isObject(obj)) {
     return `${obj}\n`;
   }
 
   const space = ' '.repeat(spaceNumber);
-  const innerSpace = ' '.repeat(spaceNumber + SPACE_NAMBER);
-
+  const innerSpace = ' '.repeat(spaceNumber + SPACE_INCREMENT);
   const diffReport = Object.entries(obj).reduce((acc, [key, value]) => {
     if (_.isObject(value)) {
-      return [...acc, `${innerSpace}    ${key}: ${getFormatObject(value, spaceNumber + SPACE_NAMBER)}`];
-    } else {
-      return [...acc, `${innerSpace}    ${key}: ${value}\n`];
+      return [...acc, `${innerSpace}    ${key}: ${getFormatObject(value, spaceNumber + SPACE_INCREMENT)}`];
     }
+    return [...acc, `${innerSpace}    ${key}: ${value}\n`];
   }, []);
-
   return ['{\n', ...diffReport, `${space}    }\n`].join('');
 };
 
 const addLineStylish = (data1, data2, diffData, spaceNumber = 0) => {
-  const diffReport = [];
   const space = ' '.repeat(spaceNumber);
-  diffData.forEach((diffValue, diffKey) => {
-    const value1 = Object.hasOwn(data1, diffKey)
-      ? data1[diffKey]
-      : 'ERROR';
-    const value2 = Object.hasOwn(data2, diffKey)
-      ? data2[diffKey]
-      : 'ERROR';
+  const innerSpace = ' '.repeat(spaceNumber + SPACE_INCREMENT);
+
+  const diffReport = Array.from(diffData.entries()).reduce((acc, [diffKey, diffValue]) => {
+    const value1 = Object.hasOwn(data1, diffKey) ? data1[diffKey] : 'ERROR';
+    const value2 = Object.hasOwn(data2, diffKey) ? data2[diffKey] : 'ERROR';
 
     if (diffValue === 'deleted') {
-      diffReport.push(`${space}  - ${diffKey}: ${getFormatObject(value1, spaceNumber)}`);
-    } else if (diffValue === 'added') {
-      diffReport.push(`${space}  + ${diffKey}: ${getFormatObject(value2, spaceNumber)}`);
-    } else if (diffValue === 'unchanged') {
-      diffReport.push(`${space}    ${diffKey}: ${getFormatObject(value1, spaceNumber)}`);
-    } else if (diffValue === 'changed') {
-      diffReport.push(`${space}  - ${diffKey}: ${getFormatObject(value1, spaceNumber)}`);
-      diffReport.push(`${space}  + ${diffKey}: ${getFormatObject(value2, spaceNumber)}`);
-    } else {
-      diffReport.push(`${space}    ${diffKey}: {\n`);
-      diffReport.push(addLineStylish(value1, value2, diffValue, spaceNumber + SPACE_NAMBER));
-      diffReport.push(`${space}    }\n`);
+      return [...acc, `${space}  - ${diffKey}: ${getFormatObject(value1, spaceNumber)}`];
     }
-  });
+    if (diffValue === 'added') {
+      return [...acc, `${space}  + ${diffKey}: ${getFormatObject(value2, spaceNumber)}`];
+    }
+    if (diffValue === 'unchanged') {
+      return [...acc, `${space}    ${diffKey}: ${getFormatObject(value1, spaceNumber)}`];
+    }
+    if (diffValue === 'changed') {
+      return [
+        ...acc,
+        `${space}  - ${diffKey}: ${getFormatObject(value1, spaceNumber)}`,
+        `${space}  + ${diffKey}: ${getFormatObject(value2, spaceNumber)}`,
+      ];
+    }
+
+    const nestedLines = addLineStylish(value1, value2, diffValue, spaceNumber + SPACE_INCREMENT);
+    return [
+      ...acc,
+      `${innerSpace}${diffKey}: {\n`,
+      nestedLines,
+      `${innerSpace}}\n`,
+    ];
+  }, []);
+
   return diffReport.join('');
 };
 
-const formatToStylish = (data1, data2, diffData) => {
-  return addStartEndStylish(addLineStylish(data1, data2, diffData));
-};
+const formatToStylish = (data1, data2, diffData) => addStartEndStylish(addLineStylish(data1, data2, diffData));
 
 export default formatToStylish;
