399f8035cfd81aa82f134dd18d7f46e88fd11ede
opifex <opifexM@outlook.com>
2024-02-16 12:53:09 -0500

    Refactor comparison logic in dataComparer.js

--- src/dataComparer.js
+++ src/dataComparer.js
@@ -1,12 +1,11 @@
 import _ from 'lodash';
 
 const generateDiffMap = (data1, data2) => {
-  const keys = [...Object.keys(data1), ...Object.keys(data2)];
-  const uniqueKeys = _.sortBy(Array.from(new Set(keys)), (key) => key);
+  const uniqueKeys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
 
   return uniqueKeys.reduce((map, key) => {
-    const hasInData1 = Object.hasOwn(data1, key);
-    const hasInData2 = Object.hasOwn(data2, key);
+    const hasInData1 = _.has(data1, key);
+    const hasInData2 = _.has(data2, key);
     if (hasInData1 && !hasInData2) {
       map.set(key, 'deleted');
       return map;
@@ -16,10 +15,9 @@ const generateDiffMap = (data1, data2) => {
       return map;
     }
 
-    const value1 = hasInData1 ? data1[key] : '';
-    const value2 = hasInData2 ? data2[key] : '';
-    const isSameValues = (hasInData1 && hasInData2) ? _.isEqual(value1, value2) : false;
-    if (isSameValues) {
+    const value1 = _.get(data1, key, '');
+    const value2 = _.get(data2, key, '');
+    if (_.isEqual(value1, value2)) {
       map.set(key, 'unchanged');
       return map;
     }
